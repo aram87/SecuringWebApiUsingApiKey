26e595cf63ef9e64587c728cece91aa385213989
Aram <aram@codingsonata.com>
2020-11-01 09:24:22 +0200

    Add project files.

---
+++ SecuringWebApiUsingApiKey/Attributes/ApiKeyAttribute.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+
+namespace SecuringWebApiUsingApiKey.Attributes
+{
+    [AttributeUsage(validOn: AttributeTargets.Class)]
+    public class ApiKeyAttribute : Attribute, IAsyncActionFilter
+    {
+        private const string APIKEYNAME = "ApiKey";
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if (!context.HttpContext.Request.Headers.TryGetValue(APIKEYNAME, out var extractedApiKey))
+            {
+                context.Result = new ContentResult()
+                {
+                    StatusCode = 401,
+                    Content = "Api Key was not provided"
+                };
+                return;
+            }
+
+            var appSettings = context.HttpContext.RequestServices.GetRequiredService<IConfiguration>();
+
+            var apiKey = appSettings.GetValue<string>(APIKEYNAME);
+
+            if (!apiKey.Equals(extractedApiKey))
+            {
+                context.Result = new ContentResult()
+                {
+                    StatusCode = 401,
+                    Content = "Unauthorized client"
+                };
+                return;
+            }
+
+            await next();
+        }
+    }
+}
---
+++ SecuringWebApiUsingApiKey/Middleware/ApiKeyMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+
+namespace SecuringWebApiUsingApiKey.Middleware
+{
+    public class ApiKeyMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private const string APIKEYNAME = "ApiKey";
+        public ApiKeyMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (!context.Request.Headers.TryGetValue(APIKEYNAME, out var extractedApiKey))
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("Api Key was not provided. (Using ApiKeyMiddleware)");
+                return;
+            }
+
+            var appSettings = context.RequestServices.GetRequiredService<IConfiguration>();
+
+            var apiKey = appSettings.GetValue<string>(APIKEYNAME);
+
+            if (!apiKey.Equals(extractedApiKey))
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("Unauthorized client. (Using ApiKeyMiddleware)");
+                return;
+            }
+
+            await _next(context);
+        }
+    }
+
+  
+}
